11626af8dabeef212d249d4b0469cb3dfe881492
Valentin Sackmann <sava1013@h-ka.de>
2023-05-15 14:32:31 +0200

    added hateoase methods

--- src/main/java/com/acme/therapeut/rest/TherapeutGetController.java
+++ src/main/java/com/acme/therapeut/rest/TherapeutGetController.java
@@ -2,18 +2,28 @@ package com.acme.therapeut.rest;
 
 import com.acme.therapeut.entity.Therapeut;
 import com.acme.therapeut.service.TherapeutReadService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.LinkRelation;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Collection;
 import java.util.UUID;
 
 import static com.acme.therapeut.rest.TherapeutGetController.REST_PATH;
+import static org.springframework.hateoas.MediaTypes.HAL_JSON_VALUE;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 /**
@@ -26,6 +36,7 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @RequestMapping(REST_PATH)
 @ResponseBody
 @RequiredArgsConstructor
+@Slf4j
 @SuppressWarnings("TrailingComment")
 public class TherapeutGetController {
     /**
@@ -46,18 +57,7 @@ public class TherapeutGetController {
      */
     private final TherapeutReadService service;
 
-
-    /**
-     * Suche anhand der Therapeut-ID als Pfad-Parameter.
-     *
-     * @param id ID des zu suchenden Therapeuten
-     * @return Gefundener Therapeut.
-     */
-    @GetMapping(path = "{id:" + ID_PATTERN + "}", produces = APPLICATION_JSON_VALUE)
-    @ApiResponse(responseCode = "200", description = "Therapeut gefunden")
-    Therapeut findById(@PathVariable final UUID id) {
-        return service.findById(id);
-    }
+    private final UriHelper uriHelper;
 
 
     /**
@@ -69,5 +69,38 @@ public class TherapeutGetController {
     @ApiResponse(responseCode = "200", description = "Therapeut gefunden")
     Collection<Therapeut> findAll() {
         return service.findAll();
+    }
+
+    /**
+     * Suche mit diversen Suchkriterien als Query-Parameter.
+     *
+     * @param suchkriterien Query-Parameter als Map.
+     * @param request Das Request-Objekt, um Links für HATEOAS zu erstellen.
+     * @return Gefundenen Therapeuten als CollectionModel.
+     */
+    @GetMapping(produces = HAL_JSON_VALUE)
+    @Operation(summary = "Suche mit Suchkriterien", tags = "Suchen")
+    @ApiResponse(responseCode = "200", description = "CollectionModel mid den Therapeuten")
+    @ApiResponse(responseCode = "404", description = "Keine Therapeuten gefunden")
+    CollectionModel<TherapeutModel> find(
+        @RequestParam @NonNull final MultiValueMap<String, String> suchkriterien,
+        final HttpServletRequest request
+    ) {
+        log.debug("find: suchkriterien={}", suchkriterien);
+
+        final var baseUri = uriHelper.getBaseUri(request).toString();
+
+        // Geschaeftslogik
+        final var models = service.find(suchkriterien)
+            .stream()
+            .map(therapeut -> {
+                final var model = new TherapeutModel(therapeut);
+                model.add(Link.of(baseUri + '/' + therapeut.getId()));
+                return model;
+            })
+            .toList();
+
+        log.debug("find: {}", models);
+        return CollectionModel.of(models);
     }
 }
---
+++ src/main/java/com/acme/therapeut/rest/TherapeutModel.java
@@ -0,0 +1,53 @@
+package com.acme.therapeut.rest;
+
+import com.acme.therapeut.entity.Adresse;
+import com.acme.therapeut.entity.GeschlechtType;
+import com.acme.therapeut.entity.TaetigkeitsbereichType;
+import com.acme.therapeut.entity.Therapeut;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.hateoas.RepresentationModel;
+import org.springframework.hateoas.server.core.Relation;
+
+import java.time.LocalDate;
+import java.util.List;
+
+/**
+ * Model-Klasse für Spring HATEOAS. @lombok.Data fasst die Annotationsn @ToString, @EqualsAndHashCode, @Getter, @Setter
+ * und @RequiredArgsConstructor zusammen.
+ *
+ * @author Valentin Sackmann
+ */
+@JsonPropertyOrder({
+    "nachname", "vorname", "email", "geburtsdatum", "geschlecht",
+    "adresse", "taetigkeitsbereiche"
+})
+@Relation(collectionRelation = "therapeuten", itemRelation = "therapeut")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true, callSuper = false)
+@Getter
+@Setter
+@ToString(callSuper = true)
+class TherapeutModel extends RepresentationModel<TherapeutModel> {
+    private final String nachname;
+    private final String vorname;
+    @EqualsAndHashCode.Include
+    private final String email;
+
+    private final LocalDate geburtsdatum;
+    private final GeschlechtType geschlecht;
+    private final Adresse adresse;
+    private final List<TaetigkeitsbereichType> taetigkeitsbereiche;
+
+    TherapeutModel(final Therapeut therapeut) {
+        nachname = therapeut.getNachname();
+        vorname = therapeut.getVorname();
+        email = therapeut.getEmail();
+        geburtsdatum = therapeut.getGeburtsdatum();
+        geschlecht = therapeut.getGeschlecht();
+        adresse = therapeut.getAdresse();
+        taetigkeitsbereiche = therapeut.getTaetigkeitsbereiche();
+    }
+}
